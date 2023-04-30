69ceaafcfb92ddc4794fa44f25328767906360d7
Wander1521 <evander.jimenez.jimenez@est.una.ac.cr>
2023-04-30 02:20:51 -0600

    (CREACIÓN) instancia de firebase
    Se creo todo lo necesario para poder utilizar el firestore

---
+++ proyecto/dataBase/firebase/firebase.ts
@@ -0,0 +1,21 @@
+import "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfHPA-Ao8hwWfn9O1wt1dmwJSkJg3oGCc",
+  authDomain: "departamentosdb.firebaseapp.com",
+  projectId: "departamentosdb",
+  storageBucket: "departamentosdb.appspot.com",
+  messagingSenderId: "725611960379",
+  appId: "1:725611960379:web:4291e70de4866e0e29a41e",
+  measurementId: "G-EMQVC3825X",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const firestore = getFirestore(app);
+
+export { firestore };
--- proyecto/src/pages/index.tsx
+++ proyecto/src/pages/index.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Header } from "../root/components/header/Header";
+import { Top } from "../root/components/header/Top";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={` bg-PrimaryGreen flex min-h-screen flex-col items-center justify-between ${inter.className}`}>
-      <header className = "bg-SecundaryGreen w-full">
-        <Header />
+      <header className = "bg-SecondaryGreen w-full h-auto">
+        <Top />
       </header>
     </main>
   );
