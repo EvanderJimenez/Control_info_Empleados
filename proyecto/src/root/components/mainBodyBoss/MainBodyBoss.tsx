a22a3aca056c775a316bd7dc44ea22f9e98e1a55
DARIOVRUNA <ramon.vargas.rojas@est.una.ac.cr>
2023-05-15 15:07:03 -0600

    Update MainBodyBoss.tsx

--- proyecto/src/root/components/mainBodyBoss/MainBodyBoss.tsx
+++ proyecto/src/root/components/mainBodyBoss/MainBodyBoss.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import BossMenu from "../bossMenu/BossMenu";
-import SearchInput from "../searchInput/SearchInput";
 import EditEmployeeSection from "../editEmployeeSection/EditEmployeeSection";
 import EmployeeTimeSection from "../employeeTimeSection/EmployeeTimeSection";
 
 
 
 export function MainBodyBoss() {
+  const [showComponent, setShowComponent] = useState(true);
+
+  const toggleComponent = () => {
+    setShowComponent(!showComponent);
+  };
   return (
     <>
-      <EmployeeTimeSection/>
+    <div className="bg-SecondaryColor flex-col w-full h-1/3 p-1 flex justify-center items-center">
+      <button className="bg-blue p-4 " onClick={toggleComponent}>
+        {showComponent ? "Go to Edit Employee Information, click here" : "Go to Employee Information, click here"}
+      </button>
+      <div className="bg-SecondaryColor flex items-center justify-center flex-col h-full w-full p-10">
+        <div>{showComponent ? <EmployeeTimeSection /> : <EditEmployeeSection/>}</div>
+      </div>
+    </div>
     </>
   );
 }
-//<EditEmployeeSection/>
