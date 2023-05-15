bbd272f5c7dbd348dc824cb9739403cdaa8aac25
Wander1521 <evander.jimenez.jimenez@est.una.ac.cr>
2023-05-15 02:57:54 -0600

    (CREACIÓN) pagina de edición de empleado

--- proyecto/src/root/components/mainBodyBoss/MainBodyBoss.tsx
+++ proyecto/src/root/components/mainBodyBoss/MainBodyBoss.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import BossMenu from "../bossMenu/BossMenu";
 import SearchInput from "../searchInput/SearchInput";
 import EditEmployeeSection from "../editEmployeeSection/EditEmployeeSection";
-
+import Prueba from "../prueba/Prueba";
 
 export function MainBodyBoss() {
   return (
     <>
       <SearchInput labelInputSeekerOne="email" labelInputSeekerTwo="text" placeholderSeekerOne="Enter the email to search" placeholderSeekerTwo="Enter the name to search" />
-      
+     <EditEmployeeSection/>
     </>
   );
 }
