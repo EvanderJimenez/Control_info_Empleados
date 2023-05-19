641c0207ba4b8b97940a2eb0c6127a8cacfbcb76
DARIOVRUNA <ramon.vargas.rojas@est.una.ac.cr>
2023-05-19 03:19:07 -0600

    (ACTUALIZACION) login cookies

--- proyecto/src/root/components/login/Login.tsx
+++ proyecto/src/root/components/login/Login.tsx
@@ -1,38 +1,31 @@
-import { useState, ChangeEvent, use } from "react";
+import { useState, ChangeEvent, useEffect } from "react";
 import ListEmployee from "../listEmployee/ListEmployee";
-import router from "next/router";
-import { LoginEP, UserData } from "../../interface/employee/";
+import { LoginEP } from "../../interface/employee/";
 import FormLogin from "./components/FormLogin";
-import { setCookie } from "cookies-next";
 import { useDispatch, useSelector } from "react-redux";
 import { StartLogin } from "@/root/redux/thunks/employee-thunk/employee.thunk";
 import { selectLogin } from "@/root/redux/selectors/employee-selector/employee.selector";
-import { RootState } from "@/root/redux/store";
-import { startGetDepartmentById } from "@/root/redux/thunks/department-thunk/department.thunk";
 import { selectGetDepartmentById } from "@/root/redux/selectors/department-selector/department.selector";
+import { startGetDepartmentById } from "@/root/redux/thunks/department-thunk/department.thunk";
+import cookiesUser from "@/root/utils/login/cookiesUser";
 
 function Login() {
   const dispatch = useDispatch();
 
-  const login = useSelector((state: RootState) => state.loginStore.loginUser);
-
-  const getDepartmentById = useSelector(
-    (state: RootState) => state.getDepartmentByIdStore.getDepartmentById
-  );
-
   const loginState = useSelector(selectLogin);
-
   const resDepart = useSelector(selectGetDepartmentById);
 
-  const [data, setData] = useState<LoginEP>(() => {
-    return {
-      email: "",
-      password: "",
-    };
+  const [data, setData] = useState<LoginEP>({
+    email: "",
+    password: "",
   });
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  const [isRegistrando, setIsRegistrando] = useState<boolean>(false);
-  const [errorEmailPass, setErrorEmailPass] = useState<any>(null);
+
+  useEffect(() => {
+    if (loginState) {
+      dispatch(startGetDepartmentById(loginState.idDepartment));
+    }
+  }, [loginState, dispatch]);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -45,42 +38,23 @@ function Login() {
     if (data.email && data.password) {
       try {
         dispatch(StartLogin(data.email, data.password));
-        console.log(loginState)
-        if (loginState) {
-          dispatch(startGetDepartmentById(loginState.idDepartment));
-
-          console.log(resDepart);
-
-          if (resDepart) {
-            const expirationDate = new Date(Date.now() + 86400 * 1000);
-            let cookieValue = "";
-
-            if (resDepart.leader !== loginState.uid) {
-              cookieValue = JSON.stringify({ logged: true, type: "employee" });
-              router.push("/home/EmployeeMain");
-            } else if (resDepart.leader === loginState.uid) {
-              cookieValue = JSON.stringify({ logged: true, type: "boss" });
-              router.push("/home/BossMain");
-            } else if (loginState.jobPosition === "Admin") {
-              cookieValue = JSON.stringify({ logged: true, type: "admin" });
-              router.push("/home/AdminMain");
-            }
-
-            setCookie("logged", cookieValue, {
-              path: "/",
-              expires: expirationDate,
-            });
-          } else {
-          }
-        } else {
-          setErrorEmailPass(true);
-          throw new Error("Error al iniciar sesión");
-        }
       } catch (error) {
         console.error(error);
       }
     }
   };
+
+  useEffect(() => {
+    if (loginState && resDepart) {
+      cookiesUser(loginState,resDepart);
+    }
+  }, [loginState, resDepart]);
+
+  useEffect(() => {
+    if (loginState && resDepart) {
+      setIsLoggedIn(true);
+    }
+  }, [loginState, resDepart]);
 
   return (
     <>
--- proyecto/src/root/utils/login/cookiesUser.ts
+++ proyecto/src/root/utils/login/cookiesUser.ts
@@ -1,58 +1,34 @@
-
-import { selectGetDepartmentById } from '@/root/redux/selectors/department-selector/department.selector';
-import { selectLogin } from '@/root/redux/selectors/employee-selector/employee.selector';
 import { setCookie } from 'cookies-next';
 import router from 'next/router';
-import { useSelector } from 'react-redux';
 
-function cookiesUser(){
-
-    const loginState = useSelector(selectLogin);
-
-    const resDepart = useSelector(selectGetDepartmentById);
-
-    if((loginState && resDepart)){
-        const expirationDate = new Date(Date.now() + 86400 * 1000);
-
-
-        if (resDepart.leader !== loginState.uid) {
-          const cookieValue = JSON.stringify({
-            logged: true,
-            type: "employee",
-          });
-
-          setCookie("logged", cookieValue, {
-            path: "/",
-            expires: expirationDate,
-          });
-
-          router.push("/home/EmployeeMain");
-        } else if (resDepart.leader === loginState.uid) {
-          const cookieValue = JSON.stringify({
-            logged: true,
-            type: "boss",
-          });
-
-          setCookie("logged", cookieValue, {
-            path: "/",
-            expires: expirationDate,
-          });
-          router.push("/home/BossMain");
-        } else if (loginState.jobPosition === "Admin") {
-          const cookieValue = JSON.stringify({
-            logged: true,
-            type: "admin",
-          });
-
-          setCookie("logged", cookieValue, {
-            path: "/",
-            expires: expirationDate,
-          });
-          router.push("/home/AdminMain");
-        }
-    }
-
+interface LoginState {
+  uid: string;
+  jobPosition: string;
 }
 
+interface ResDepartState {
+  leader: string;
+}
 
-export default cookiesUser
+function cookiesUser(loginState: LoginState | undefined, resDepart: ResDepartState | undefined) {
+
+  if (loginState && resDepart) {
+    const expirationDate = new Date(Date.now() + 86400 * 1000);
+    let cookieValue = '';
+
+    if (resDepart.leader !== loginState.uid) {
+      cookieValue = JSON.stringify({ logged: true, type: "employee" });
+      router.push("/home/EmployeeMain");
+    } else if (resDepart.leader === loginState.uid) {
+      cookieValue = JSON.stringify({ logged: true, type: "boss" });
+      router.push("/home/BossMain");
+    } else if (loginState.jobPosition === "Admin") {
+      cookieValue = JSON.stringify({ logged: true, type: "admin" });
+      router.push("/home/AdminMain");
+    }
+
+    setCookie("logged", cookieValue, { path: "/", expires: expirationDate });
+  }
+}
+
+export default cookiesUser;
