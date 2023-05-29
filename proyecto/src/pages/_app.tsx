4d8aae953484d4b84356b2ded808aa9ef8eda2cf
DARIOVRUNA <ramon.vargas.rojas@est.una.ac.cr>
2023-05-29 14:51:35 -0600

    (CREACION ) icono y titulo de la pagina

--- proyecto/src/pages/_app.tsx
+++ proyecto/src/pages/_app.tsx
@@ -5,51 +5,55 @@ import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import ApplicationStore from "@/root/redux/store";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
+import { useEffect } from "react";
 const toastConfig = {
-
   success: {
-    duration:2000,
+    duration: 2000,
     style: {
       background: "#B3C890",
-      color: 'white',
+      color: "white",
     },
     iconTheme: {
-      primary: 'white',
-      secondary: 'green',
+      primary: "white",
+      secondary: "green",
     },
   },
 
   error: {
-    duration:2000,
+    duration: 2000,
     style: {
       background: "#CD0404",
-      color: 'white',
+      color: "white",
     },
     iconTheme: {
-      primary: 'white',
-      secondary: 'red',
+      primary: "white",
+      secondary: "red",
     },
   },
 
   loading: {
-    duration:2000,
+    duration: 2000,
     style: {
       background: "#9DB2BF",
-      color: 'white',
+      color: "white",
     },
     iconTheme: {
-      primary: 'white',
-      secondary: 'blue',
+      primary: "white",
+      secondary: "blue",
     },
   },
-
 };
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={ApplicationStore}>
+      <Head>
+        <title>CR Home</title>
+        <link rel="icon" href="/principal.ico" />
+      </Head>
       <Component {...pageProps} />
-      <Toaster position= "top-center" toastOptions={toastConfig} />
+      <Toaster position="top-center" toastOptions={toastConfig} />
     </Provider>
   );
 }
--- proyecto/src/pages/index.tsx
+++ proyecto/src/pages/index.tsx
@@ -1,25 +1,18 @@
 import React, { useEffect } from "react";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
-import Head from "next/head";
 
 const InvoicePage: NextPage = () => {
   const router = useRouter();
-
-  <Head>
-    <title>CR Home</title>
-    <link rel="icon" href="/favicon.ico" />
-  </Head>;
-
+  
   useEffect(() => {
-    //What is it the idea of this?
     router.push("./home/");
   }, [router]);
 
-  return <>  <Head>
-  <title>CR Home</title>
-  <link rel="icon" href="/favicon.ico" />
-</Head>;</>;
+  return (
+    <>
+    </>
+  );
 };
 
 export default InvoicePage;
