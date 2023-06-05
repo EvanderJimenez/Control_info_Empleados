4f676edb572da3113b1354bedda10198f8c17ea6
Wander1521 <evander.jimenez.jimenez@est.una.ac.cr>
2023-06-04 21:44:41 -0600

    (ACTUALIZACIÓN) index inicial

--- proyecto/src/pages/home/index.tsx
+++ proyecto/src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import LoginPage from "@/Layout/LoginPage";
+import { BrandsClock } from "@/root/components/brandsClock/BrandsClock";
 import MethodsBrands from "@/root/components/brandsClock/brandsEmployee/methodsBrands/MethodsBrands";
+import { ListDepartment } from "@/root/components/listDepartment/ListDeparment";
 import MainForm from "@/root/components/mainForm/MainForm";
 
 export default function index() {
   return (
     <LoginPage>
-      <MainForm />
+      <MethodsBrands />
     </LoginPage>
   );
 }
